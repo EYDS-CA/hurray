81c3dccce2c0319096efa8525517857ab36cb890
Ashton Meuser <ashtonmeuser@gmail.com>
2020-09-18 15:17:41 -0700

    Handle posting for day or listing for month

--- handler.js
+++ handler.js
@@ -14,13 +14,28 @@ const app = new App({
   receiver: expressReceiver,
   processBeforeResponse: true,
 });
+const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
-const theView = (content) => ({
+const titleCase = (s) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`;
+
+const queryView = (month, blocks) => ({
   type: 'modal',
-  callback_id: 'shoutout_submitted',
   title: {
     type: 'plain_text',
-    text: 'Hurray',
+    text: `${titleCase(month)} Workiversaries`,
+  },
+  close: {
+    type: 'plain_text',
+    text: 'Close',
+  },
+  blocks,
+});
+
+const errorView = (month) => ({
+  type: 'modal',
+  title: {
+    type: 'plain_text',
+    text: 'Error',
   },
   close: {
     type: 'plain_text',
@@ -28,33 +43,58 @@ const theView = (content) => ({
   },
   blocks: [{
     type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: content,
-    },
+    text: { type: 'mrkdwn', text: `Invalid month: "${month}"` },
   }],
 });
 
-app.command('/hurray', async ({
-  body,
-  ack,
-  context,
-}) => {
+const employeeBlocks = (employees) => {
+  if (employees.length <= 0) {
+    return [{
+      type: 'section',
+      text: { type: 'mrkdwn', text: 'No employees with workiversaries this period' },
+    }];
+  }
+  return employees.map((employee) => ({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*${employee.preferred}* ${employee.hireDate.format('MMMM D')}`,
+    },
+  }));
+};
+
+app.command('/hurray', async ({ body, ack, context }) => {
   try {
     await ack();
+    console.log(body);
 
     const data = await getSheetContent(process.env.SHEET_ID, process.env.SHEET_RANGE);
     const employees = data[0].values.map(employeeFromRow);
-    const workiversaries = workiversariesByMonth(employees, 'September');
-    const content = JSON.stringify(workiversaries.map((x) => x.name));
 
-    await app.client.views.open({
-      token: context.botToken,
-      trigger_id: body.trigger_id,
-      view: theView(content),
-    });
-  } catch (e) {
-    console.log(e);
+    if (typeof body.text !== 'string' || body.text.trim() === '') { // Empty
+      const workiversaries = workiversariesByDates(employees, reportingDates());
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: process.env.SLACK_CHANNEL_ID,
+        text: { blocks: employeeBlocks(workiversaries) },
+      });
+    } else if (months.includes(body.text.trim().toLowerCase())) { // Valid month
+      const month = body.text.trim();
+      const workiversaries = workiversariesByMonth(employees, month);
+      await app.client.views.open({
+        token: context.botToken,
+        trigger_id: body.trigger_id,
+        view: queryView(month, employeeBlocks(workiversaries)),
+      });
+    } else { // Invalid month
+      await app.client.views.open({
+        token: context.botToken,
+        trigger_id: body.trigger_id,
+        view: errorView(body.text),
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
 
